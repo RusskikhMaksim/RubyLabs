d25699b6cc7ab8ee269e91eca45409b29f20aeb8
Russkikh Maksim <36906216+RusskikhMaksim@users.noreply.github.com>
2021-01-20 21:22:34 +0700

    Update csv_actions_spec.rb

--- lab2/spec/csv_actions_spec.rb
+++ lab2/spec/csv_actions_spec.rb
@@ -2,32 +2,30 @@ require_relative '../lib/csv_actions'
 require 'csv'
 
 RSpec.describe CsvActions do
-  let(:csv_actions_obj) { CsvActions.new("test.csv") }
+  let(:csv_actions_obj) { CsvActions.new('test.csv') }
   describe '.find_min' do
     it do
-      csv_actions_obj.find_min
-      expect(csv_actions_obj.min).to eq 3.8
+      expect(csv_actions_obj.find_min).to eq 3.8
     end
   end
 
   describe '.find_max' do
     it do
-      csv_actions_obj.find_max
-      expect(csv_actions_obj.max).to eq 1872.3
+      expect(csv_actions_obj.find_max).to eq 1872.3
     end
   end
 
   describe '.find_avg' do
     it do
       csv_actions_obj.find_avg
-      expect(csv_actions_obj.avg).to eq 154.24
+      expect(csv_actions_obj.avg).to eq 303.83
     end
   end
 
   describe '.find_corrected_sv' do
     it do
       csv_actions_obj.find_corrected_sv
-      expect(csv_actions_obj.corrected_sv).to eq 215_229.73
+      expect(csv_actions_obj.corrected_sv).to eq 218_718.38
     end
   end
 end
