484330682e0730ab16d74897d886dc091f533e8c
Russkikh Maksim <36906216+RusskikhMaksim@users.noreply.github.com>
2021-01-20 21:23:16 +0700

    Update csv_actions.rb

--- lab2/lib/csv_actions.rb
+++ lab2/lib/csv_actions.rb
@@ -1,59 +1,35 @@
 class CsvActions
-  attr_reader :max, :min, :avg, :corrected_sv
+  attr_reader :avg, :corrected_sv
   attr_writer :file
-
 
   def initialize(filename)
     @file = CSV.read("#{__dir__}/#{filename}")
+    @data = []
+    @file.each do |row|
+      row.each do |elem|
+        @data << elem.to_f if row.index(elem) != 0
+      end
+    end
+    @count_of_elements = @data.count
   end
 
   def find_max
-    @max = -1
-    @file.each do |items|
-      items.each do |item|
-        @max = item.to_f if item.to_f > @max
-      end
-    end
+    @data.max
   end
 
   def find_min
-    @min = 99_999
-    @file.each do |items|
-      items.each do |item|
-        @min = item.to_f if item.to_f < @min && items.index(item) != 0
-      end
-    end
+    @data.min
   end
 
   def find_avg
-    avg_value = 0
-    avg_per_row = 0
-    items_count = 0
-    @file.each do |items|
-      items_count = items.count - 1
-      items.each do |item|
-        avg_per_row += item.to_f
-      end
-      avg_per_row /= items_count
-      avg_value += avg_per_row
-    end
-    @avg = (avg_value / @file.count).round(2)
+    sum_of_all_elements = @data.inject { |sum, elem| sum.to_f + elem.to_f }
+    @avg = (sum_of_all_elements / @count_of_elements).round(2)
   end
 
   def find_corrected_sv
-    row_sum = 0
     find_avg
-    elems_count = @file.count * (@file[0].count - 1)
-    row_sum = find_corrected_sv_action(elems_count, row_sum)
-    @corrected_sv = ((row_sum * elems_count) / (elems_count - 1)).round(2)
-  end
-
-  def find_corrected_sv_action(elems_count, row_sum)
-    @file.each do |items|
-      items.each do |item|
-        row_sum += ((item.to_f - @avg)**2).to_f
-      end
-    end
-    row_sum /= elems_count
+    sum_of_subtracts = @data.inject { |sum, elem| sum + (elem - @avg)**2 }
+    divider = @count_of_elements - 1
+    @corrected_sv = (sum_of_subtracts / divider).round(2)
   end
 end
